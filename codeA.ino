30c5a0d9cc89c0b3620d8d7186728492a4b35e7f
Vineeth-Narayan <57552723+Vineeth-Narayan@users.noreply.github.com>
2020-07-01 16:35:37 +0530

    Add files via upload

--- codeA.ino
+++ codeA.ino
@@ -1,8 +1,21 @@
 #include <NewPing.h>
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+
+//----------- Enter you Wi-Fi Details---------//
+char ssid[] = "xxxxxxxx"; //SSID
+char pass[] = "yyyyyyyy"; // Password
+//-------------------------------------------//
 
 NewPing sonar1(A0, A1, 100);
 NewPing sonar2(A2, A3, 100);
 NewPing sonar3(A4, A5, 100);
+
+WiFiClient  client;
+
+unsigned long myChannelField = 123456; // Channel ID
+const int ChannelField = 1; // Which channel to write data
+const char * myWriteAPIKey = "xxxxxxxxxxx"; // Your write API Key
 
 int dis1,dis2,dis3;
 int i;
@@ -12,17 +25,71 @@ int y=0;
 int x1,x2,y1,y2;
 
 //Motor A
-const int ip1  = 4;    // Pin 15 of L293D IC
-const int ip2  = 5;
+const int ip2  = 3;    // Pin 15 of L293D IC
+const int ip1  = 4;
 
 // Pin 10 of L293D IC
 //Motor B
-const int ip3  = 8;   // Pin  7 of L293D IC
-const int ip4  = 7;
+const int ip4  = 5;   // Pin  7 of L293D IC
+const int ip3  = 6;
 
 // Pin  2 of L293D IC
 
-void distance()
+
+void setup() 
+{
+    pinMode(ip1, OUTPUT);
+    pinMode(ip2, OUTPUT);
+    pinMode(ip3, OUTPUT);
+    pinMode(ip4, OUTPUT);  
+    WiFi.mode(WIFI_STA);
+    ThingSpeak.begin(client);
+    Serial.begin(9600);
+    
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, pass);
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+    
+  distance();
+  if(dis2>30)
+  {
+    left();
+    delay(1000);
+    dir=dir+1;
+    dir=dir%4;
+    stoop();
+  }
+  else if(dis1>100)
+  {
+    forward();
+    delay(1000);
+    stoop();
+    plot(dir);
+  }
+  else 
+  {
+    right();
+    delay(1000);
+    dir=dir-1;
+    dir=dir%4;
+    stoop();
+  }
+ }
+
+  void distance()
 {
   dis1=sonar1.ping_cm();
   dis2=sonar2.ping_cm();
@@ -61,6 +128,14 @@ void right()
       digitalWrite(ip2,HIGH);
       
       digitalWrite(ip3,HIGH);
+      digitalWrite(ip4,LOW);
+}
+void stoop()
+{
+
+      digitalWrite(ip1,LOW);
+      digitalWrite(ip2,LOW);
+      digitalWrite(ip3,LOW);
       digitalWrite(ip4,LOW);
 }
 
@@ -174,40 +249,3 @@ void plot(int dir)
     
   }
 }
-void setup() 
-{
-    pinMode(ip1, OUTPUT);
-    pinMode(ip2, OUTPUT);
-    pinMode(ip3, OUTPUT);
-    pinMode(ip4, OUTPUT);  
-    
-    Serial.begin(9600);
-    
-}
-
-void loop()
-{
-  distance();
-  if(dis2>30)
-  {
-    left();
-    delay(100);
-    dir=dir+1;
-    dir=dir%4;
-  }
-  else if(dis1>100)
-  {
-    forward();
-    delay(100);
-    plot(dir);
-  }
-  else 
-  {
-    right();
-    delay(100);
-    dir=dir-1;
-    dir=dir%4;
-  }
- }
-
-  
